b6106ba34b69d2aa3fb85767b6c58953568571cb
yo-fenix <alejandro.iglesias@trifenix.io>
2021-02-27 19:29:47 -0300

    Se agrega gitigonore y se agregan clases de trifenix connect

---
+++ search-mdl/ISearchOperations.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using trifenix.connect.mdm.entity_model;
+
+namespace trifenix.connect.search_mdl
+{
+
+    /// <summary>
+    /// interface para obtener un entitySearch desde un motor de base de datos
+    /// </summary>
+    /// <typeparam name="T">El modelo incluye el campo de tipo geo, la forma de este campo depende de cada tipo de base de datos</typeparam>
+    public interface ISearchEntity<T>
+    {
+        /// <summary>
+        /// obtiene una entidad desde una implementación de base de datos del modelo.
+        /// </summary>
+        /// <param name="entityKind">tipo de entidad a recuperar</param>
+        /// <param name="idEntity">identificar de la entidad</param>
+        /// <returns></returns>
+        IEntitySearch<T> GetEntity(int entityKind, string idEntity);
+
+
+    }
+}
