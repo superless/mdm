0b4e0acaa9cf00b9064ca4b0f1e385ec24aab8a2
yo-fenix <alejandro.iglesias@trifenix.io>
2021-03-07 20:10:29 -0300

    Se agrega toda la información necesaria para la creación del nuevo metadata en el modelo
    Revisar con el anterior, para ver las diferencias mientras se llena desde mdm-cli (trifenix.connect hará el trabajo)

--- ts-model/ModelMetaData.cs
+++ ts-model/ModelMetaData.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using trifenix.versions.model;
 
 namespace trifenix.connect.mdm.ts_model
 {
@@ -13,47 +13,38 @@ namespace trifenix.connect.mdm.ts_model
         public string Version { get; set; }
 
         /// <summary>
+        /// Información detallada de la historia de versiones.
+        /// </summary>
+        public VersionStructure VersionStructure { get; set; }
+
+
+        /// <summary>
         /// Coleccion de metadata de cada entidad del modelo
         /// </summary>
         public EntityMetadata[] Indexes { get; set; }
+
+
+        /// <summary>
+        /// Estructura de Filtros globales, indicando de que manera
+        /// llegar a el proceso principal del filtro (Barracks).
+        /// </summary>
+        public  GlobalFilters GlobalFilters { get; set; }
 
         /// <summary>
         /// Descripciones de enumeraciones útiles.
         /// </summary>
         public EnumDescription[] EnumDescriptions { get; set; } = new EnumDescription[] { };
-    }
-
-
-    /// <summary>
-    /// Descripción de enumeración
-    /// </summary>
-    public class EnumDescription {
 
         /// <summary>
-        /// Nombre de la enumeración
+        /// Documentación principal, repositorio de documentación de cada índice
         /// </summary>
-        public string Name { get; set; }
+        public MainDocumentation MainDocumentation { get; set; }
+
 
         /// <summary>
-        /// Descripciones de cada item de una enumeración.
+        /// Menú generado con la metadata.
         /// </summary>
-        public ItemEnumDescription[] Descriptions { get; set; } = new ItemEnumDescription[] { };
-    }
+        public GroupMenu[] Menu { get; set; }
 
-
-    /// <summary>
-    /// descripción de un item de enumeración.
-    /// </summary>
-    public class ItemEnumDescription {
-
-        /// <summary>
-        /// índice de una enumeración
-        /// </summary>
-        public int Index { get; set; }
-
-        /// <summary>
-        /// Descripción de una enumeración, encontrado en el parámetro Description o el nombre del item de la enumeración.
-        /// </summary>
-        public string Description { get; set; }
     }
 }
