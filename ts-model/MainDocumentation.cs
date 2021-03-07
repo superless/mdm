7298aa9a976a606a82c2a68aab79e3c552781ad1
yo-fenix <alejandro.iglesias@trifenix.io>
2021-03-07 20:13:45 -0300

    Estructura de documentación
    SI bien la mayoría de los elementos, tienen una documentación, esta estructura es global en el modelo, para consultar desde cualquier lugar por la documentación de un índice

---
+++ ts-model/MainDocumentation.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace trifenix.connect.mdm.ts_model
+{
+    /// <summary>
+    /// Documentación principal
+    /// </summary>
+    public class MainDocumentation{
+
+        /// <summary>
+        /// Documentación de Strings
+        /// </summary>
+        public Dictionary<int, EntitySearchDisplayInfo> Strs { get; set; }
+
+        /// <summary>
+        /// Documentación de Nums
+        /// </summary>
+        public Dictionary<int, EntitySearchDisplayInfo> Nums { get; set; }
+
+        /// <summary>
+        /// Documentación de Geos
+        /// </summary>
+        public Dictionary<int, EntitySearchDisplayInfo> Geos { get; set; }
+
+        /// <summary>
+        /// Documentación de Fechas
+        /// </summary>
+        public Dictionary<int, EntitySearchDisplayInfo> Dates { get; set; }
+
+        /// <summary>
+        /// Documentación de booleanos
+        /// </summary>
+        public Dictionary<int, EntitySearchDisplayInfo> Bools { get; set; }
+
+
+        /// <summary>
+        /// Documentación de Enums
+        /// </summary>
+        public Dictionary<int, EntitySearchDisplayInfo> Enums { get; set; }
+
+        /// <summary>
+        /// Documentación de Entidades
+        /// </summary>
+        public Dictionary<int, EntitySearchDisplayInfo> Rels { get; set; }
+    }
+}
