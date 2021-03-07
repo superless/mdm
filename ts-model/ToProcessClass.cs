0357185df9816e84482596a839464f490918ebc1
yo-fenix <alejandro.iglesias@trifenix.io>
2021-03-07 20:08:08 -0300

    Ruta al proceso
    toProcessClass indica la ruta para llegar a un elemento o entidad, de esta manera se puede saber como filtrar desde una entidad a otra.

---
+++ ts-model/ToProcessClass.cs
@@ -0,0 +1,25 @@
+﻿namespace trifenix.connect.mdm.ts_model
+{
+    /// <summary>
+    /// Determina la ruta de filtros para el proceso que da origin a todos los filtros.
+    /// En este caso, al ser barrack el corazón, usará esta clase para determinar la ruta.
+    /// </summary>
+    public class ToProcessClass {
+
+        /// <summary>
+        /// Determina la ruta para llegar a la entidad de proceso principal (ej. barrack).
+        /// </summary>
+        public PathToFiltersValue[] PathToProcess { get; set; }
+
+        /// <summary>
+        /// Clase a llegar, ej : Barrack
+        /// </summary>
+        public string ProcessNameClass { get; set; }
+
+
+        /// <summary>
+        /// índice de la clase, para usarlo en la consulta.
+        /// </summary>
+        public int IndexProcessClass { get; set; }
+    }
+}
