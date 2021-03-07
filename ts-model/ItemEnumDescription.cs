06d992172c43d558600eed5175d474faf0a0ef4a
yo-fenix <alejandro.iglesias@trifenix.io>
2021-03-07 20:14:27 -0300

    Describe un item de un menú
    Para la metadata.

---
+++ ts-model/ItemEnumDescription.cs
@@ -0,0 +1,18 @@
+﻿namespace trifenix.connect.mdm.ts_model
+{
+    /// <summary>
+    /// descripción de un item de enumeración.
+    /// </summary>
+    public class ItemEnumDescription {
+
+        /// <summary>
+        /// índice de una enumeración
+        /// </summary>
+        public int Index { get; set; }
+
+        /// <summary>
+        /// Descripción de una enumeración, encontrado en el parámetro Description o el nombre del item de la enumeración.
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
