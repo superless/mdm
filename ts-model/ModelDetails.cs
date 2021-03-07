c550fc76004ac3530013e2cfe1fd6b409f02b885
yo-fenix <alejandro.iglesias@trifenix.io>
2021-03-07 20:11:54 -0300

    Otra forma de mostrar los datos
    ModelDetails mantiene una estructura para mostrar las propiedades de una clase.

---
+++ ts-model/ModelDetails.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace trifenix.connect.mdm.ts_model
+{
+    /// <summary>
+    /// otra vista de las propiedades
+    /// </summary>
+    public class ModelDetails {
+        // <summary>
+        /// Cada una de las propiedades
+        /// </summary>
+        public Dictionary<string, PropertyMetadata> PropsDetails { get; set; }
+
+
+        /// <summary>
+        /// Cada una de las propiedades de tipo related.
+        /// </summary>
+        public Dictionary<string, RelatedPropertyMetadata> RelatedDetails { get; set; }
+
+        /// <summary>
+        /// Recursividad con los campos de tipo objeto con sus propias propiedades
+        /// </summary>
+        public Dictionary<string, ModelDetails> RelatedInputs { get; set; }
+    }
+
+
+}
