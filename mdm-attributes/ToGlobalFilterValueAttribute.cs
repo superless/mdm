120a8c45df8f2232e8cbdbbe9f74e5747262416b
yo-fenix <alejandro.iglesias@trifenix.io>
2021-03-07 20:42:07 -0300

    Ruta para obtener el valor de un filtro
    Determina una ruta para obtener el valor de un filtro, por ejemplo, filtramos centro de costo, pero queremos el idSeason.

---
+++ mdm-attributes/ToGlobalFilterValueAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace trifenix.connect.mdm_attributes
+{
+    /// <summary>
+    /// Determina si una propiedad es usada para llegar al valor 
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+    public class ToGlobalFilterValueAttribute : Attribute
+    {
+        /// <summary>
+        /// Determina a que tipo de elemento pertenece el valor.
+        /// Si una clase que es de tipo filtro no tiene GlobalFilterValue, usará el mismo valor.
+        /// </summary>
+        /// <param name="type"></param>
+        public ToGlobalFilterValueAttribute(Type type)
+        {
+            Type = type;
+        }
+
+        /// <summary>
+        /// Filtro principal
+        /// </summary>
+        public Type Type { get; }
+    }
+}
