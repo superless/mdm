2b2bffab65fca6aa1259539970556f84609cb4ca
yo-fenix <alejandro.iglesias@trifenix.io>
2021-03-07 20:37:58 -0300

    Se toma de trifenix connect
    Se necesitan las rutas de borrado.

---
+++ model/DeleteItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace trifenix.connect.model
+{
+    /// <summary>
+    /// Elementos de una coleccion que contiene el tipo de documento(Barrack, order, etc)
+    /// y la propiedad a ser usada para la eliminacion/desactivacion
+    /// </summary>
+    public class DeleteItem
+    {
+        /// <summary>
+        /// Tipo de documento(Barrack, order, etc)
+        /// </summary>
+        public Type DocumentType { get; set; }
+
+        /// <summary>
+        /// Propiedades de un documento
+        /// </summary>
+        public string Property { get; set; }
+    }
+
+}
