ddcc670b0eec9c3b0e5938acb034b84fdfecbf81
yo-fenix <alejandro.iglesias@trifenix.io>
2021-03-07 20:15:41 -0300

    Input Related
    Este inputRelated hereda de RelatedPropertyMetadata e incluye la propiedad que representa en el modelo y si es de referencia (si no, significa que es un objeto con elementos dentro).

---
+++ ts-model/InputPropRelatedDetails.cs
@@ -0,0 +1,20 @@
+﻿namespace trifenix.connect.mdm.ts_model
+{
+    /// <summary>
+    /// Detalles de una propiedad related
+    /// </summary>
+    public class InputPropRelatedDetails : RelatedPropertyMetadata
+    {  
+        /// <summary>
+        /// Nombre del mismo campo en el modelo.
+        /// </summary>
+        public string ModelPropName { get; set; }
+
+        /// <summary>
+        /// Declara si el input es de referencia o contiene los valores de ingreso.
+        /// </summary>
+        public bool IsReference { get; set; }
+    }
+
+
+}
