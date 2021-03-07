9593fd6b393f9b7a90503a408f3f6bb1b068f75e
yo-fenix <alejandro.iglesias@trifenix.io>
2021-03-07 19:27:06 -0300

    Se cambia el namespace y la carpeta de uniqueAttribute
    Queda junto a las demás atributos de validaciones

---
+++ validation-attributes/UniqueAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace trifenix.connect.mdm.validation_attributes
+{
+    //Solo asignar a valores primitivos
+    /// <summary>
+    /// Determina si un campo debe ser único, esto permitirá
+    /// validarlo en el servidor y entregar metadata al cliente
+    /// para que también lo valide.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class UniqueAttribute : Attribute { }
+}
