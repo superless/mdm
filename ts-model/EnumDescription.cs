22384a7a1a6619d82712676d4190541408ee4887
yo-fenix <alejandro.iglesias@trifenix.io>
2021-03-07 20:36:48 -0300

    Descripción de enums
    Detalles de enumeración, de todos los elementos encontrados.

---
+++ ts-model/EnumDescription.cs
@@ -0,0 +1,18 @@
+﻿namespace trifenix.connect.mdm.ts_model
+{
+    /// <summary>
+    /// Descripción de enumeración
+    /// </summary>
+    public class EnumDescription {
+
+        /// <summary>
+        /// Nombre de la enumeración
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Descripciones de cada item de una enumeración.
+        /// </summary>
+        public ItemEnumDescription[] Descriptions { get; set; } = new ItemEnumDescription[] { };
+    }
+}
