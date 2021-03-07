4d70a7ed99ea19414f2b58adb301f0aa6202169d
yo-fenix <alejandro.iglesias@trifenix.io>
2021-03-07 20:19:31 -0300

    Item de un menú para metadata

---
+++ ts-model/GroupMenuViewer.cs
@@ -0,0 +1,24 @@
+﻿namespace trifenix.connect.mdm.ts_model
+{
+    /// <summary>
+    /// Representa un item de menú y sus links.
+    /// </summary>
+    public class GroupMenuViewer 
+    {
+        /// <summary>
+        /// Nombre del menú raíz.
+        /// ej: mantenedores.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Colección de items de menú.
+        /// </summary>
+        public GroupMenuItem[] Items { get; set; }
+
+        /// <summary>
+        /// Sub Menús.
+        /// </summary>
+        public GroupMenuViewer[] GroupMenu { get; set; }
+    }
+}
