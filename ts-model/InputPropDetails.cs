84a002ae9d5667a55a6507db47cbb15f197e295e
yo-fenix <alejandro.iglesias@trifenix.io>
2021-03-07 20:16:33 -0300

    Detalles de una propiedad input
    En la metadata irá todo lo que corresponde al modelo más el nombre de la propiedad del modelo, del cual está conectada.

---
+++ ts-model/InputPropDetails.cs
@@ -0,0 +1,16 @@
+﻿namespace trifenix.connect.mdm.ts_model
+{
+    /// <summary>
+    /// Detalles de una propiedad que no sea related.
+    /// </summary>
+    public class InputPropDetails : PropertyMetadata {
+
+       
+        /// <summary>
+        /// Nombre del mismo campo en el modelo.
+        /// </summary>
+        public string ModelPropName { get; set; }
+    }
+
+
+}
