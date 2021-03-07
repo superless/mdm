3021b366f77a82e48adc8d061e19fe4d33455d01
yo-fenix <alejandro.iglesias@trifenix.io>
2021-03-07 20:41:13 -0300

    Atributo ToProcess
    Determina las rutas para llegar a cada elemento

---
+++ mdm-attributes/ToProcessAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace trifenix.connect.mdm_attributes
+{
+    /// <summary>
+    /// Determina si una propiedad es usada para llegar a un proceso principal
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+    public class ToProcessAttribute : Attribute
+    {
+        /// <summary>
+        /// Determina a que tipo de elemento pertenece el valor.
+        /// </summary>
+        /// <param name="type">Tipo de procesos (Barrack, por ej.)</param>
+        public ToProcessAttribute(Type[] type)
+        {
+            Type = type;
+        }
+
+        /// <summary>
+        /// Obtiene el tipo del proceso (Barrack)
+        /// </summary>
+        public Type[] Type { get; }
+    }
+}
