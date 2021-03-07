f7af0d03877e7ec04ca129b81c0e98c3dabe68a9
yo-fenix <alejandro.iglesias@trifenix.io>
2021-03-07 20:19:50 -0300

    Item con link de un groupMenuViewer

---
+++ ts-model/GroupMenuItem.cs
@@ -0,0 +1,27 @@
+﻿namespace trifenix.connect.mdm.ts_model
+{
+    /// <summary>
+    /// Determina el valor de un item de menú.
+    /// </summary>
+    public class GroupMenuItem {
+
+        /// <summary>
+        /// Ruta del link del menú.
+        /// </summary>
+        public string PathName { get; set; }
+
+        /// <summary>
+        /// Nombre del grupo menu item, en este caso sería el nombre de la clase, en la documentación.
+        /// </summary>
+        public string Name { get; set; }
+
+
+        /// <summary>
+        /// Nombre Corto del grupo menu item, en este caso sería el nombre de la clase, en la documentación.
+        /// </summary>
+        public string ShortName { get; set; }
+
+
+
+    }
+}
