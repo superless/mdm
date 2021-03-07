04044a2dcedaca963d4c5d3c12046a67b7df37fb
yo-fenix <alejandro.iglesias@trifenix.io>
2021-03-07 19:27:55 -0300

    Ruta para llegar al valor del filtro
    Indica la ruta para llegar al valor de un filtro
    por ejemplo, cual es la ruta para llegar al idSeason desde Cost-center

---
+++ ts-model/ToValue.cs
@@ -0,0 +1,13 @@
+﻿namespace trifenix.connect.mdm.ts_model
+{
+    /// <summary>
+    /// Ruta para llegar al valor, por ejemplo, desde cost-center a idSeason.
+    /// </summary>
+    public class ToValue {
+
+        /// <summary>
+        /// Ruta para llegar al valor de un filtro.
+        /// </summary>
+        public PathToFiltersValue[] FiltersToValue { get; set; }
+    }
+}
