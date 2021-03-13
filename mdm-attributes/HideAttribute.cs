1b482205df956f2723aefe8542090c7bfe39b301
yo-fenix <alejandro.iglesias@trifenix.io>
2021-03-13 20:36:23 -0300

    Se agrega hide atribute

---
+++ mdm-attributes/HideAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace trifenix.connect.mdm_attributes
+{
+    /// <summary>
+    /// Oculta un campo.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class HideAttribute : Attribute
+    {
+        /// <summary>
+        /// Inicializa
+        /// </summary>
+        public HideAttribute()
+        {
+            
+        }
+
+    }
+}
