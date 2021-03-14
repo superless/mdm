7083d584f22ec16b5b56392b54454fa50701bc29
yo-fenix <alejandro.iglesias@trifenix.io>
2021-03-13 22:34:15 -0300

    Se agrega HideFront

---
+++ mdm-attributes/HideFrontAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace trifenix.connect.mdm_attributes
+{
+    /// <summary>
+    /// Oculta un campo.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class HideFrontAttribute : Attribute
+    {
+        /// <summary>
+        /// Inicializa
+        /// </summary>
+        public HideFrontAttribute()
+        {
+
+        }
+
+    }
+}
