d5deb1547801cdde550e77278b48cb7fe1bf4070
yo-fenix <alejandro.iglesias@trifenix.io>
2021-03-19 00:50:33 -0300

    input enum

---
+++ ts-model/InputPropEnumDetails.cs
@@ -0,0 +1,22 @@
+﻿namespace trifenix.connect.mdm.ts_model
+{
+    /// <summary>
+    /// Detalles de una propiedad que no sea related.
+    /// </summary>
+    public class InputPropEnumDetails : PropertyMetadadataEnum
+    {
+
+
+        /// <summary>
+        /// Nombre del mismo campo en el modelo.
+        /// </summary>
+        public string ModelPropName { get; set; }
+
+        /// <summary>
+        /// Nombre de la clase input
+        /// </summary>
+        public string ClassInput { get; set; }
+    }
+
+
+}
