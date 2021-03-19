a73696ce5710827adee23b079c8a1d1aedebee06
yo-fenix <alejandro.iglesias@trifenix.io>
2021-03-19 00:49:13 -0300

    Corrección de namespaces

---
+++ ts-model/enums/Validation.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace trifenix.connect.ts_model.enums
+{
+    public enum Validation {
+
+        [Description("Sin Validación")]
+        NOVALIDATION = 0,
+        [Description("Requerido")]
+        REQUIRED = 1,
+        [Description("único")]
+        UNIQUE = 2,
+        [Description("Referencia")]
+        REFERENCE = 3,
+        [Description("Compleja")]
+        Complex = 4,
+    }
+}
+
+
+
+
